e2d8d99f76e75f94387f316db224a8f231b88930
alansilva-pixel <alan.silva@alunos.ufersa.edu.br>
2026-06-02 14:56:37 -0300

    Corrige tela branca no login

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import { Link } from "react-router-dom"
 import api from "../services/api"
 import BrandLogo from "../components/BrandLogo"
 import ModalAviso from "../components/ModalAviso"
